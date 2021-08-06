1a5b6dc100a8b74044d291378c0470c27f6c705c
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-08-06 07:06:18 -0300

    Uso subject no teste

--- exemplo_rspec/spec/calculator/calculator_spec.rb
+++ exemplo_rspec/spec/calculator/calculator_spec.rb
@@ -1,10 +1,13 @@
 require 'calculator'
 describe Calculator do
-  it 'sum method for 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(5,2)
-    expect(result).to eq(7)
+  context 'use sum method for 2 numbers' do
+    it 'with positive numbers' do
+      result = subject.sum(5,2)
+      expect(result).to eq(7)
+    end
+    it 'with negative numbers' do
+      result = subject.sum(5,-12)
+      expect(result).to eq(-7)
+    end
   end
-
-
 end
