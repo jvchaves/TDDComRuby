74a261647c555afb17ecc695537ef3f273eaec7e
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-08-09 20:33:52 -0300

    Trabalho com matchers para arrays

---
+++ exemplo_rspec/spec/matchers/arrays_spec.rb
@@ -0,0 +1,12 @@
+describe Array.new([1,2,3,4,5]) do
+  it 'Include' do
+    expect(subject).to include(2)
+  end
+  it 'Match array' do
+    expect(subject).to match_array([1,2,3,4,5])
+  end
+  it 'contain exactly' do
+    expect(subject).to contain_exactly(1,2,3,4,5)
+  end
+
+end
