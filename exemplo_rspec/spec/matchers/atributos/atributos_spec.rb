472b87f6126cd5742c97e64fbd65dce9464b5980
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-08-12 17:50:56 -0300

    Trabalho com around

--- exemplo_rspec/spec/matchers/atributos/atributos_spec.rb
+++ exemplo_rspec/spec/matchers/atributos/atributos_spec.rb
@@ -1,14 +1,23 @@
 require 'pessoa'
 describe 'Atributos' do
-  before(:all) do
+  around(:each) do |teste|
     puts 'Antes'
     @pessoa = Pessoa.new
-  end
-  after(:each) do
+    teste.run
     @pessoa.nome = 'Sem nome'
     puts 'Depois'
     puts @pessoa.nome
+
   end
+  # before(:all) do
+  #   puts 'Antes'
+  #   @pessoa = Pessoa.new
+  # end
+  # after(:each) do
+  #   @pessoa.nome = 'Sem nome'
+  #   puts 'Depois'
+  #   puts @pessoa.nome
+  # end
   it 'have_attributes' do
     @pessoa.nome = 'Joao'
     @pessoa.idade = 21
