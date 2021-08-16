2a9304cef1a6c12c9cadc5b5c63fd5a5bb83da1c
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-08-16 16:00:55 -0300

    Trabalho com matcher de output padrão para erros

--- exemplo_rspec/spec/matchers/output/output_spec.rb
+++ exemplo_rspec/spec/matchers/output/output_spec.rb
@@ -2,5 +2,8 @@ describe 'Matcher Output' do
   it{expect{puts 'jv'}.to output.to_stdout}
   it{expect{print 'jv'}.to output('jv').to_stdout}
   it{expect{puts 'jv'}.to output(/jv/).to_stdout}
-
+  #OUTPUT de erro
+  it{expect{warn "jv"}.to output.to_stderr}
+  it{expect{warn "jv chaves"}.to output(/jv/).to_stderr}
+  it{expect{warn "jv"}.to output(/jv/).to_stderr}
 end
