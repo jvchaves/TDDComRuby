dee2e129d984cdf1f0b91e3a8ae2ae427b6204b6
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-08-16 16:13:47 -0300

    Implemento a negação do matcher

--- exemplo_rspec/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec/spec/matchers/arrays/arrays_spec.rb
@@ -1,3 +1,4 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
 describe Array.new([1,2,3,4,5]) do
   it 'Include' do
     expect(subject).to include(2)
@@ -8,5 +9,5 @@ describe Array.new([1,2,3,4,5]) do
   it 'contain exactly' do
     expect(subject).to contain_exactly(1,2,3,4,5)
   end
-
+  it {expect(subject).to exclude(6)}
 end
--- exemplo_rspec/spec/matchers/be_within/be_within_spec.rb
+++ exemplo_rspec/spec/matchers/be_within/be_within_spec.rb
@@ -1,4 +1,5 @@
+RSpec::Matchers.define_negated_matcher :be_not_within, :be_within
 describe 'be_within' do
   it {expect(12.5).to be_within(0.5).of(12)}
-
+  it {expect(11.4).to be_not_within(0.5).of(12)}
 end
