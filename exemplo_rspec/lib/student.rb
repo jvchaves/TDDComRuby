505c89ecbf8f76812268a05523cad34c2df0000a
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-08-18 07:56:45 -0300

    Implemento stubs para criar methods falsos

--- exemplo_rspec/lib/student.rb
+++ exemplo_rspec/lib/student.rb
@@ -1,6 +1,8 @@
 class Student
   attr_accessor :name,:email
   def has_finished?(course)
+  end
+  def foo(args)
 
   end
 end
--- exemplo_rspec/spec/stubs/stubs_spec.rb
+++ exemplo_rspec/spec/stubs/stubs_spec.rb
@@ -9,6 +9,31 @@ describe 'Stubs' do
     course_finished = student.has_finished?(course)
     expect(course_finished).to eq(true)
   end
+  it 'Argumentos dinamicos' do
+    student = Student.new
+    allow(student).to receive(:foo) do |arg|
+      if arg == :helo
+        'Olá'
+      elsif arg == :hi
+        'Hi'
+      end
+    end
+    expect(student.foo(:helo)).to eq('Olá')
+    expect(student.foo(:hi)).to eq('Hi')
+  end
+  it 'Qualquer instancia' do
+    student = Student.new
+    other_student = Student.new
+    allow_any_instance_of(Student).to receive(:foo).and_return(true)
+    expect(student.foo(:helo)).to be_truthy
+    expect(other_student.foo(:hi)).to be_truthy
+  end
+  it 'Erros' do
+    student = Student.new
 
+    allow(student).to receive(:foo).and_raise(RuntimeError)
+    expect { student.foo(:helo) }.to raise_error(RuntimeError)
+
+  end
 
 end
